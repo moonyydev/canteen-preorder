4e83fc8e1a2c376927bcea8c0f8210206a084824
moonyydev <moonydev@lunardawn.xyz>
2025-04-27 16:20:01 +0200

    tests: add a multi-user environment test

--- tests/test_backend.py
+++ tests/test_backend.py
@@ -10,3 +10,12 @@ def test_backend_create_and_login_user():
     user1 = backend.create_user("test_user", "test_user@gmail.com", "test123pass", True)
     user2 = backend.login("test_user@gmail.com", "test123pass")
     assert user1 == user2
+
+def test_backend_create_and_login_multi_user():
+    backend = PreorderBackend(":memory:")
+    backend.create_user("test_user", "test_user@gmail.com", "test123pass", True)
+    backend.create_user("user2", "2nduser@gmail.com", "somepassword", False)
+    user1 = backend.create_user("userthree", "userthethird@gmail.com", "wowpass", False)
+    backend.create_user("userV", "fifthuser@gmail.com", "unexpectedpassword", False)
+    user2 = backend.login("userthethird@gmail.com", "wowpass")
+    assert user1 == user2
