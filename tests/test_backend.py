d8fec53c87f8e3875b4b148cfc71ceb752a5c99f
moonyydev <moonydev@lunardawn.xyz>
2025-04-27 17:12:33 +0200

    tests: add meal testing collection before order testing collection in tests

--- tests/test_backend.py
+++ tests/test_backend.py
@@ -88,17 +88,20 @@ def order_testing_collection(backend: PreorderBackend) -> list[Order]:
 def test_backend_create_order():
     backend = testing_backend()
     user_testing_collection(backend)
+    meal_testing_collection(backend)
     order_testing_collection(backend)
 
 def test_backend_get_orders():
     backend = testing_backend()
     user_testing_collection(backend)
+    meal_testing_collection(backend)
     orders = order_testing_collection(backend)
     assert orders == backend.get_orders()
 
 def test_backend_get_order():
     backend = testing_backend()
     user_testing_collection(backend)
+    meal_testing_collection(backend)
     order1 = order_testing_collection(backend)[1]
     order2 = backend.get_order(order1.order_id)
     assert order1 == order2
