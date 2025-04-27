1b80df3073de2bd53df1f344e027bcdc540d158c
moonyydev <moonydev@lunardawn.xyz>
2025-04-27 14:10:09 +0200

    backend: first revision of API

--- src/canteen_preorder/backend/__init__.py
+++ src/canteen_preorder/backend/__init__.py
@@ -1,4 +1,5 @@
 import sqlite3
+from canteen_preorder.common import Meal, Id, User, Cost, OrderItem, Order
 
 class PreorderBackend:
     def __init__(self, db_path: str = "canteen.db") -> None:
@@ -38,3 +39,44 @@ class PreorderBackend:
         """)
 
         self.db.commit()
+
+    # USERS
+    def login(self, email: str, password: str) -> User:
+        pass
+    
+    def get_user(self, user_id: Id) -> User:
+        pass
+
+    # Staff Only
+    def create_user(self, name: str, email: str, password: str, staff: bool = False) -> User:
+        pass
+    
+    # MEALS
+    def get_meals(self) -> list[Meal]:
+        pass
+
+    def get_meal(self, meal_id: Id) -> Meal:
+        pass
+
+    # Staff Only
+    def create_meal(self, name: str, cost: Cost, category: int, stock: int, available: bool = True) -> Meal:
+        pass
+
+    def update_meal_stock(self, meal_id: Id, stock: int) -> None:
+        pass
+
+    def update_meal_cost(self, meal_id: Id, cost: Cost) -> None:
+        pass
+    
+    def update_meal_availability(self, meal_id: Id, available: bool = False) -> None:
+        pass
+
+    # ORDERS
+    def get_orders(self) -> list[Order]:
+        pass
+    
+    def get_order(self, order_id: Id) -> Order:
+        pass
+
+    def create_order(self, user_id: Id, items: list[OrderItem]) -> Order:
+        pass
