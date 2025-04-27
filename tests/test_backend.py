5cc7ec0b7d15c9911371d1fba2435d3f1e640c27
moonyydev <moonydev@lunardawn.xyz>
2025-04-27 16:17:53 +0200

    tests: rename test_backend_create_user to test_backend_create_and_login_user

--- tests/test_backend.py
+++ tests/test_backend.py
@@ -5,7 +5,7 @@ from canteen_preorder.backend import PreorderBackend
 def test_backend_init():
     PreorderBackend(":memory:")
 
-def test_backend_create_user():
+def test_backend_create_and_login_user():
     backend = PreorderBackend(":memory:")
     user1 = backend.create_user("test_user", "test_user@gmail.com", "test123pass", True)
     user2 = backend.login("test_user@gmail.com", "test123pass")
