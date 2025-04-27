7fe95bf8223f55508444025f93e4d1caaa72ec10
moonyydev <moonydev@lunardawn.xyz>
2025-04-27 14:10:09 +0200

    tests: create backend init test

---
+++ tests/test_backend.py
@@ -0,0 +1,7 @@
+import pytest
+from sqlite3 import DatabaseError
+from canteen_preorder.backend import PreorderBackend
+
+def test_backend_init():
+    with pytest.raises(DatabaseError):
+        PreorderBackend(":memory:")
