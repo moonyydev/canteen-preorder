b093643945231ce2bf59a92ed8d2be4455aadf9b
moonyydev <moonydev@lunardawn.xyz>
2025-04-27 16:11:32 +0200

    tests: add create user test

--- tests/test_backend.py
+++ tests/test_backend.py
@@ -4,3 +4,9 @@ from canteen_preorder.backend import PreorderBackend
 
 def test_backend_init():
     PreorderBackend(":memory:")
+
+def test_backend_create_user():
+    backend = PreorderBackend(":memory:")
+    user1 = backend.create_user("test_user", "test_user@gmail.com", "test123pass", True)
+    user2 = backend.login("test_user@gmail.com", "test123pass")
+    assert user1 == user2
