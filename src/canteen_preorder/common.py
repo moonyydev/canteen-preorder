76d99de348baeccbc6d8f62dab788c16cfdf5259
moonyydev <moonydev@lunardawn.xyz>
2025-04-29 22:42:59 +0200

    common: add docs

--- src/canteen_preorder/common.py
+++ src/canteen_preorder/common.py
@@ -1,15 +1,15 @@
 from typing import NamedTuple
 from enum import Enum
 
-type Id = int
-type Cost = int
-type OrderItem = tuple[Id, int]
+type Id = int # the id of an object (in the database)
+type Cost = int # 2-point fixed decimal cost, eg. 100 is 1.00, 451 is 4.51
+type OrderItem = tuple[Id, int] # the id of a meal ordered and the quantity
 
 class User(NamedTuple):
     user_id: Id
     name: str
     email: str
-    staff: bool = False
+    staff: bool = False # whether or not the user is part of the staff
 
 class Category(Enum):
     BREAKFAST = 0
@@ -22,10 +22,10 @@ class Meal(NamedTuple):
     cost: Cost
     category: Category
     stock: int
-    available: bool = True
+    available: bool = True # wther it's available to buy, if this is false, a meal is  classified as discontinued
 
 class Order(NamedTuple):
     order_id: Id
     user_id: Id
-    order_time: int
+    order_time: int # the time the order happened at, as a unix timestamp
     items: list[OrderItem]
